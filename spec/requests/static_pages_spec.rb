da01ab2917df616bbdf8c97b59afec189f4c88ad
Juraj Machac <jurajmchc@gmail.com>
2012-07-28 20:36:57 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,13 +8,36 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+
+    it "should have the title 'Sample App | Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Sample App | Home")
+    end
   end
 
   describe "Help page" do
 
-    if "should have the content 'Sample App Help'" do
-      visit '/static_pages/home'
+    it "should have the content 'Sample App Help'" do
+      visit '/static_pages/help'
       page.should have_content('Sample App Help')
+    end
+
+    it "should have the title 'Sample App | Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Sample App | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+
+    it "should have the title 'Sample App | About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Sample App | About Us")
     end
   end
 end
